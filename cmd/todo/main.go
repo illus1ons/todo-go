79c2586d2d599154f4d281e8cf6650bfe027d550
Youngbae Yoo <youngbae.u@gmail.com>
2022-07-24 17:30:11 +0900

    Add todo task stdin 으로 입력

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -1,7 +1,95 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"errors"
+	"flag"
+	"fmt"
+	todo "github.com/illus1ons/todo-go"
+	"io"
+	"os"
+	"strings"
+)
+
+const (
+	todoFile = "./todos.json"
+)
 
 func main() {
-	fmt.Println("Hello, world")
+	add := flag.Bool("add", false, "TODO 추가")
+	complete := flag.Int("complete", 0, "TODO 완료 표시")
+	del := flag.Int("del", 0, "TODO 삭제")
+	list := flag.Bool("list", false, "TODO 전체 목록")
+	flag.Parse()
+
+	todos := &todo.Todos{}
+
+	if err := todos.Load(todoFile); err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+
+	switch {
+	case *add:
+		task, err := getInput(os.Stdin, flag.Args()...)
+		if err != nil {
+			fmt.Fprintln(os.Stdout, err.Error())
+			os.Exit(1)
+		}
+		todos.Add(task)
+		err = todos.Store(todoFile)
+		if err != nil {
+			fmt.Fprintln(os.Stdout, err.Error())
+			os.Exit(1)
+		}
+	case *complete > 0:
+		err := todos.Complete(*complete)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+
+		err = todos.Store(todoFile)
+		if err != nil {
+			fmt.Fprintln(os.Stdout, err.Error())
+			os.Exit(1)
+		}
+	case *del > 0:
+		err := todos.Delete(*del)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+
+		err = todos.Store(todoFile)
+		if err != nil {
+			fmt.Fprintln(os.Stdout, err.Error())
+			os.Exit(1)
+		}
+	case *list:
+		todos.Print()
+	default:
+		fmt.Fprintln(os.Stdout, "invalid command")
+		os.Exit(0)
+	}
+}
+
+func getInput(r io.Reader, args ...string) (string, error) {
+	if len(args) > 0 {
+		return strings.Join(args, " "), nil
+	}
+
+	scanner := bufio.NewScanner(r)
+	scanner.Scan()
+	if err := scanner.Err(); err != nil {
+		return "", err
+	}
+
+	text := scanner.Text()
+
+	if len(text) == 0 {
+		return "", errors.New("empty todo is not allowed")
+	}
+
+	return text, nil
 }
